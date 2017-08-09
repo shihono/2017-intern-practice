5660cbb46673eadd0cbb76dacba5fc4c58c65257
hono shirai <shirai@honoMacBook-Air.local>
2017-08-09 09:10:33 +0900

    do 4_1

--- html_jinja/4_1html.py
+++ html_jinja/4_1html.py
@@ -1,14 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+
+# テンプレートファイルを指定
 env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
 tpl = env.get_template('template.html')
 
-foods = []
-foods.append({'name':u'ラーメン', 'price':400})
-foods.append({'name':u'焼き飯',   'price':500})
-foods.append({'name':u'天津飯',   'price':600})
+# テンプレートへ挿入するデータの作成
+time = datetime.now()
 
-html = tpl.render({'shop':u'悟空軒', 'foods':foods})
-print ('Content-Type: text/html; charset=utf-8\n')
-print( html.encode('utf-8'))
+# テンプレートへの挿入
+html = tpl.render({'time': time})
+print(html)
+
+# ファイルへの書き込み
+
+tmpfile = open("generate.html", 'w')  # 書き込みモードで開く
+tmpfile.write(html)
+tmpfile.close()
